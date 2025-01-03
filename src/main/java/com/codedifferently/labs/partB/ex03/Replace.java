dbb9e980cc5eecf749d5d6713cf7611e8a20154f
londyy123 <londynnista1128@gmail.com>
2025-01-03 12:29:49 -0500

    replacing "r"
    replaced all characters with lowercase r to uppercase R

--- src/main/java/com/codedifferently/labs/partB/ex03/Replace.java
+++ src/main/java/com/codedifferently/labs/partB/ex03/Replace.java
@@ -5,7 +5,11 @@ public class Replace {
         String response = "";
         String sentence = "Using String replace to replace character";
         /** Your code goes here*/
-
+        //Utilizing the built in replace method, create a new String that holds an updated version of the sentence
+        //with every lowercase r transformed into uppercase. Print out this new string.
+        String replace = "Using String replace to replace character";
+        String newString = sentence.replaceAll("r", "R");
+        response += newString;
 
         return response;
     }
