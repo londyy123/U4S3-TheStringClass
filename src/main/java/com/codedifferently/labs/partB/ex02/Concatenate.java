d43c9d261f813eb03e4f27bb8d38954db20f5e1b
londyy123 <londynnista1128@gmail.com>
2025-01-03 12:19:10 -0500

    strings & concatenation
    Made two strings and combined them for a response and printed

--- src/main/java/com/codedifferently/labs/partB/ex02/Concatenate.java
+++ src/main/java/com/codedifferently/labs/partB/ex02/Concatenate.java
@@ -4,7 +4,13 @@ public class Concatenate {
     public static String concatenateStrings() {
             String response = "";
         /* Your code goes here*/
+        //Create two different variables that hold text - a first name and a last name
+        //Create a third variable that combines both of the variables
+        //Print out the variable that contains the concatenation
 
+        String myString = "Abe";
+        myString = myString + " Lincoln";
+        response += myString;
         return response;
     }
 
